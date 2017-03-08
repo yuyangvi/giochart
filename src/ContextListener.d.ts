0eb08cd3d2056ff77ae914c005a9e8950e0cefe5
yuyangvi <yuyangvi@gmail.com>
2017-03-08 20:36:02 +0800

    delete d.ts

--- src/ContextListener.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="react" />
-import * as React from "react";
-import { SingleChartProps } from "./ChartProps";
-declare class ContextListener extends React.Component<SingleChartProps, any> {
-    private static contextTypes;
-    render(): JSX.Element;
-    private generateChartParams();
-}
-export default ContextListener;
