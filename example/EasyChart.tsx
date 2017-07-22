0d2aa14d2ffc017dd7ca488ad4cc92d6c0cc92d3
yuyangvi <yuyangvi@gmail.com>
2017-07-22 15:43:18 +0800

    retentionSeletor

--- example/EasyChart.tsx
+++ example/EasyChart.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import { DrawParamsProps, Source} from "../src/ChartProps";
 import Chart from "../src/Chart";
-import { retentionSourceSelector } from "../src/utils";
+
 const retentionData: Source = [
   { retention: 2045, type: "ww"}, // 23
   { retention: 1045, type: "dssasdga/zkjdhakj a说的/卡萨丁和卡号卡时间的话/asjdsd"},
@@ -10,6 +10,7 @@ const retentionData: Source = [
 
 const chartParams: DrawParamsProps = {
   adjust: "stack",
+  aggregator: { values: [4510] },
   chartType: "bar",
   columns: [
     { id: "type", name: "type", isDim: true, isRate: false },
--- example/index.tsx
+++ example/index.tsx
@@ -1,15 +1,16 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-import DonutChart from "./DonutChart"
 
 /*
+import DonutChart from "./DonutChart"
 import Retention from "./Retention";
 import DualChart from"./DualChart";
 import EasyChart from "./EasyChart";
 import ComparisonChart from "./ComparisonChart";
 */
+import RetentionGrid from "./RetentionGrid";
 
 ReactDOM.render(
-  <DonutChart />,
+  <RetentionGrid />,
   document.getElementById("example")
 );
